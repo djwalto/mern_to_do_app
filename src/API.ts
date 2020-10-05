9b59d82f5fd4ca76d404af02dd5d08ef6c57f706
David Walton <davidwalton6772@gmail.com>
2020-10-04 19:13:47 -0500

    update

--- src/API.ts
+++ src/API.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from "axios";
 
 const baseUrl: string = "http://localhost:4000";
 
-export const geTodos = async (): Promise<AxiosResponse<ApiDataType>> => {
+export const getTodos = async (): Promise<AxiosResponse<ApiDataType>> => {
     try {
         const todos: AxiosResponse<ApiDataType> = await axios.get(
             baseUrl + "/todos"
