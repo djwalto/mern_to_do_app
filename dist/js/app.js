f2d8561e16a3829913b2fc228e793c0d19d400bc
David Walton <davidwalton6772@gmail.com>
2020-10-05 11:40:40 -0500

    debugging

--- dist/js/app.js
+++ dist/js/app.js
@@ -12,13 +12,12 @@ const PORT = process.env.PORT || 4000;
 app.use(cors_1.default());
 app.use(routes_1.default);
 // 
-const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.b7brn.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustertodo.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
 mongoose_1.default.set("useFindAndModify", false);
 mongoose_1.default
     .connect(uri, options)
     .then(() => app.listen(PORT, () => console.log(`Server running on localhost ${PORT}`)))
     .catch(error => {
-
-        console.log(error, process.env.MONGO_USER, process.env.MONGO_PASSWORD, process.env.MONGO_DB);
-    });
+    throw error;
+});
